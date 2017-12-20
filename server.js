013a76378e06d56196465b8f03b64d30533faf80
Richard5277 <richard.dev@icloud.com>
2017-12-19 19:31:26 -0500

    lala

--- server.js
+++ server.js
@@ -1,25 +1,18 @@
 
 ///
 ///
-
-var app = require('express')();
+var express = require('express')
+var app = express();
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
-// var io = require('socket.io')(http);
-
 var mongoose = require('mongoose')
-mongoose.Promise = require('bluebird')
-var mongoURI = "mongodb://52.237.32.43:27017/sushi4u"
-var options = { promiseLibrary: require('bluebird'), useMongoClient: true }
+var port = 8080;
+var methodOverride = require('method-override')
+var config = require('./config/config')
+var LOCAL_DB_URL = config.LOCAL_DB_URL
 
-mongoose.connect(mongoURI, options, function () { /* ... */})
-.then(() => {
-    console.log("mongodb connection open")
-})
-.catch(err => { 
-    console.error('App starting error:', err.stack);
-    process.exit(1);
-})
+mongoose.connect("mongodb://52.233.44.67:27017/sushi4u" || LOCAL_DB_URL, {useMongoClient: true})
+mongoose.connection.on('error', console.error.bind(console, 'Mongo error:'))
 
 app.set('port', 8080);
 app.set('views', __dirname + '/views');
@@ -28,7 +21,8 @@ app.set('view engine', 'pug');
 app.use(morgan('dev'));
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(methodOverride('X-HTTP-Method-Override'))
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -38,9 +32,12 @@ app.use(function(req, res, next) {
 
 require('./routes/index.js')(app);
 
-var http = require('http').Server(app);
-http.listen(8080, function(){
-   console.log("Listening on 8080");
-});
+app.listen(port)
+console.log("Listening on 8080");
+
+// var http = require('http').Server(app);
+// http.listen(8080, function(){
+//    console.log("Listening on 8080");
+// });
 
 
